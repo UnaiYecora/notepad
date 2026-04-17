abbde9ce1124561ecb8c6212a8700c996735cd03
UnaiYecora <unaiyecora@gmail.com>
2026-04-17 14:25:59 +0100

    fix: icon mask

--- vite.config.js
+++ vite.config.js
@@ -34,13 +34,11 @@ export default defineConfig({
 						src: '/icon-192-192.png', // You will need to add these dummy icons to your /public folder later
 						sizes: '192x192',
 						type: 'image/png',
-						purpose: 'any maskable'
 					},
 					{
 						src: '/icon-512-512.png',
 						sizes: '512x512',
 						type: 'image/png',
-						purpose: 'any maskable'
 					}
 				]
 			}
